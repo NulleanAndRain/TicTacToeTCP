c475b11c169b7890f3016c360d9d18e11cea1296
Nullean <mr000lean@gmail.com>
2021-05-28 02:06:17 +0400

    copy client and server from metanit

---
+++ TestConsoleClient/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TestConsoleClient {
+	#region Client
+	//class Program {
+	//	static void Main(string[] args) {
+	//		Console.WriteLine("Hello World!");
+	//	}
+	//}
+
+	class Client {
+		TcpClient client;
+		NetworkStream stream;
+		Thread listeningThread;
+
+		Queue<string> recievedData;
+
+		private void Connect(string IP, string portStr, string name) {
+			if (!string.IsNullOrWhiteSpace(IP) &&
+				int.TryParse(portStr, out int port) &&
+				string.IsNullOrWhiteSpace(name)) {
+				try {
+
+					//todo: connect to server
+
+					client = new TcpClient(IP, port);
+					stream = client.GetStream();
+
+					listeningThread = new Thread(ReadData);
+					listeningThread.Start();
+
+					WriteData(name);
+
+					//_ConnectionStatus.Content = "Connected";
+					Console.WriteLine("Connected");
+
+					while (true) {
+						var msg = recievedData.Dequeue();
+						if (msg != null) {
+							//test_area.Content += Environment.NewLine + msg;
+							Console.WriteLine(msg);
+						}
+						Thread.Sleep(16);
+					}
+				} catch (SocketException e) {
+					Console.WriteLine($"SocketException: {e}");
+				} catch (Exception e) {
+					Console.WriteLine($"Exception: {e.Message}");
+				} finally {
+					Disconnect();
+				}
+			}
+		}
+
+		void ReadData() {
+			StringBuilder builder = new StringBuilder();
+			while (client.Connected) {
+				// todo: data reading
+				try {
+					byte[] buffer = new byte[256];
+					int bytes;
+					do {
+						bytes = stream.Read(buffer, 0, buffer.Length);
+						builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+					} while (stream.DataAvailable);
+
+					recievedData.Enqueue(builder.ToString());
+					builder.Clear();
+				} catch {
+					Disconnect();
+				}
+
+				Thread.Sleep(16);
+			}
+		}
+
+		void WriteData(string data) {
+			if (stream != null && stream.CanWrite) {
+				byte[] bytes = Encoding.Unicode.GetBytes(data);
+				stream.Write(bytes, 0, bytes.Length);
+			}
+		}
+
+		private void Disconnect() {
+			if (client != null) {
+				// todo: disconect from server
+				stream?.Close();
+				client.Close();
+				client = null;
+				stream = null;
+
+				listeningThread?.Interrupt();
+				listeningThread = null;
+			}
+			//_Connect_Btn.Content = "Connect";
+		}
+
+	}
+	#endregion
+
+	class Program {
+		static string userName;
+		private const string host = "127.0.0.1";
+		private const int port = 8888;
+		static TcpClient client;
+		static NetworkStream stream;
+
+		static void Main(string[] args) {
+			Console.Write("Введите свое имя: ");
+			userName = Console.ReadLine();
+			client = new TcpClient();
+			try {
+				client.Connect(host, port); //подключение клиента
+				stream = client.GetStream(); // получаем поток
+
+				string message = userName;
+				byte[] data = Encoding.Unicode.GetBytes(message);
+				stream.Write(data, 0, data.Length);
+
+				// запускаем новый поток для получения данных
+				Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+				receiveThread.Start(); //старт потока
+				Console.WriteLine("Добро пожаловать, {0}", userName);
+				SendMessage();
+			} catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			} finally {
+				Disconnect();
+			}
+		}
+		// отправка сообщений
+		static void SendMessage() {
+			Console.WriteLine("Введите сообщение: ");
+
+			while (true) {
+				string message = Console.ReadLine();
+				byte[] data = Encoding.Unicode.GetBytes(message);
+				stream.Write(data, 0, data.Length);
+			}
+		}
+		// получение сообщений
+		static void ReceiveMessage() {
+			while (true) {
+				try {
+					byte[] data = new byte[64]; // буфер для получаемых данных
+					StringBuilder builder = new StringBuilder();
+					int bytes = 0;
+					do {
+						bytes = stream.Read(data, 0, data.Length);
+						builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+					}
+					while (stream.DataAvailable);
+
+					string message = builder.ToString();
+					Console.WriteLine(message);//вывод сообщения
+				} catch {
+					Console.WriteLine("Подключение прервано!"); //соединение было прервано
+					Console.ReadLine();
+					Disconnect();
+				}
+			}
+		}
+
+		static void Disconnect() {
+			if (stream != null)
+				stream.Close();//отключение потока
+			if (client != null)
+				client.Close();//отключение клиента
+			Environment.Exit(0); //завершение процесса
+		}
+	}
+}
--- TicTacToeTCPServer/Program.cs
+++ TicTacToeTCPServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,12 +10,13 @@ namespace TicTacToeTCPServer
 {
 	class Program {
 		static void Main(string[] args) {
-			Server server = new Server("127.0.0.1", 8888);
-			server.Start();
+			ServerObject server = new ServerObject();
+			server.Listen("127.0.0.1", 8888);
 		}
 
 	}
 
+	#region Server alt
 	class Server {
 		TcpListener listener;
 		List<Client> clients;
@@ -30,11 +32,13 @@ namespace TicTacToeTCPServer
 				Console.WriteLine("Сервер запущен. Ожидание подключений...");
 
 				while (true) {
-					//TcpClient tcpClient = listener.AcceptTcpClient();
+					TcpClient tcpClient = listener.AcceptTcpClient();
 
-					//Client clientObject = new Client(tcpClient, this);
-					//Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
-					//clientThread.Start();
+					Client clientObject = new Client(tcpClient, this);
+					Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+					clientThread.Start();
+
+					clients.Add(clientObject);
 					Thread.Sleep(5);
 				}
 			} catch (Exception ex) {
@@ -51,55 +55,185 @@ namespace TicTacToeTCPServer
 			Environment.Exit(0); //завершение процесса
 		}
 
-		/*
-		public void Start() {
-			listener.Start();
-			listeningThread = new Thread(ReadData);
-			listeningThread.Start();
+		void Listen() {
 			while (true) {
-				if (recievedData.Count > 0) {
-					Console.WriteLine(recievedData.Dequeue());
-				}
-				Thread.Sleep(16);
+
+				Thread.Sleep(5);
 			}
 		}
 
-		/*
-		void ReadData() {
-			StringBuilder builder = new StringBuilder();
-			while (true) {
-				// todo: data reading
-				try {
-					byte[] buffer = new byte[256];
-					int bytes;
-					do {
-						bytes = stream.Read(buffer, 0, buffer.Length);
-						builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
-					} while (stream.DataAvailable);
+		//public void Start() {
+		//	listener.Start();
+		//	listeningThread = new Thread(ReadData);
+		//	listeningThread.Start();
+		//	while (true) {
+		//		if (recievedData.Count > 0) {
+		//			Console.WriteLine(recievedData.Dequeue());
+		//		}
+		//		Thread.Sleep(16);
+		//	}
+		//}
 
-					recievedData.Enqueue(builder.ToString());
-					builder.Clear();
-				} catch {
-					//Disconnect();
-				}
+		//void ReadData() {
+		//	StringBuilder builder = new StringBuilder();
+		//	while (true) {
+		//		// todo: data reading
+		//		try {
+		//			byte[] buffer = new byte[256];
+		//			int bytes;
+		//			do {
+		//				bytes = stream.Read(buffer, 0, buffer.Length);
+		//				builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+		//			} while (stream.DataAvailable);
 
-				Thread.Sleep(16);
-			}
-		}*/
+		//			recievedData.Enqueue(builder.ToString());
+		//			builder.Clear();
+		//		} catch {
+		//			//Disconnect();
+		//		}
+
+		//		Thread.Sleep(16);
+		//	}
+		//}
 	}
 
-	class Client {
-		public Client(TcpClient client, Server server)
-        {
+    class Client {
+        public Client(TcpClient client, Server server) { }
+        public void Process() { }
+        public void Close() { }
+    }
 
-        }
-		public void Process()
-        {
+	#endregion
 
-        }
-		public void Close()
-        {
+	#region ServerObject
 
+	public class ServerObject {
+        static TcpListener tcpListener; // сервер для прослушивания
+        List<ClientObject> clients = new List<ClientObject>(); // все подключения
+
+        protected internal void AddConnection(ClientObject clientObject) {
+            clients.Add(clientObject);
         }
-	}
+        protected internal void RemoveConnection(string id) {
+            // получаем по id закрытое подключение
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            // и удаляем его из списка подключений
+            if (client != null)
+                clients.Remove(client);
+        }
+        // прослушивание входящих подключений
+        protected internal void Listen(string IP, int port) {
+            try {
+                tcpListener = new TcpListener(IPAddress.Parse(IP), port);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true) {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        // трансляция сообщения подключенным клиентам
+        protected internal void BroadcastMessage(string message, string id) {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++) {
+                if (clients[i].Id != id) // если id клиента не равно id отправляющего
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); //передача данных
+                }
+            }
+        }
+        // отключение всех клиентов
+        protected internal void Disconnect() {
+            tcpListener.Stop(); //остановка сервера
+
+            for (int i = 0; i < clients.Count; i++) {
+                clients[i].Close(); //отключение клиента
+            }
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+    #endregion
+
+    #region ClientObject
+
+    public class ClientObject {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+        string userName;
+        TcpClient client;
+        ServerObject server; // объект сервера
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject) {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+
+        public void Process() {
+            try {
+                Stream = client.GetStream();
+                // получаем имя пользователя
+                string message = GetMessage();
+                userName = message;
+
+                message = userName + " вошел в чат";
+                // посылаем сообщение о входе в чат всем подключенным пользователям
+                server.BroadcastMessage(message, this.Id);
+                Console.WriteLine(message);
+                // в бесконечном цикле получаем сообщения от клиента
+                while (true) {
+                    try {
+                        message = GetMessage();
+                        message = String.Format("{0}: {1}", userName, message);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                    } catch {
+                        message = String.Format("{0}: покинул чат", userName);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            } finally {
+                // в случае выхода из цикла закрываем ресурсы
+                server.RemoveConnection(this.Id);
+                Close();
+            }
+        }
+
+        // чтение входящего сообщения и преобразование в строку
+        private string GetMessage() {
+            byte[] data = new byte[64]; // буфер для получаемых данных
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+
+        // закрытие подключения
+        protected internal void Close() {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+	#endregion
 }
